f80054bc7587c4d781bf994fc3f55c5d826c1489
JAK0723-GitHub <29014804+JAK0723@users.noreply.github.com>
2018-08-26 07:22:08 +0100

    Corrected card_from_letters implementation

--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -1,6 +1,9 @@
 #include "cards.h"
 
 int main(void) {
-  card_t test1 = {5, 0};
+  card_t test1 = {2, 0};
   print_card(test1);
+
+  card_t test2 = card_from_letters('5', 's');
+  print_card(test2);
 }
