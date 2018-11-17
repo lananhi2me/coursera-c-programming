9c679f2a7682479bc6073fc44c6102e11dd4f492
JAK0723 <29014804+JAK0723@users.noreply.github.com>
2018-11-17 09:15:41 +0000

    First attempt for c3prj1_deck

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -2,18 +2,53 @@
 #include <stdlib.h>
 #include <assert.h>
 #include "deck.h"
-void print_hand(deck_t * hand){
 
+void print_hand(deck_t* hand){
+  for (int i = 0; i < hand->n_cards; i++) {
+    print_hand(*(hand->cards[i]));
+  }
 }
 
-int deck_contains(deck_t * d, card_t c) {
+int deck_contains(deck_t* d, card_t c) {
+  for (int i = 0; i < d->n_cards; i++) {
+    if (*(d->cards[i]) == c) {
+      return 1;
+    }
+  }
   return 0;
 }
 
-void shuffle(deck_t * d){
-
+void shuffle(deck_t* d){
+  for (int i = 0; i < d->n_cards - 1; i++) {
+    long k = random() % d->n_cards;
+    card_t* temp = deck_t->cards[k];
+    deck_t->cards[k] = deck_t->cards[i];
+    deck_t->cards[i] = temp;
+  }
 }
 
-void assert_full_deck(deck_t * d) {
-
+void assert_full_deck(deck_t* d) {
+  if (!d || !d->cards) {
+    printf("Null deck pointer");
+    exit(EXIT_FAILURE);
+  }
+  if (d->n_cards != 52) {
+    printf("Deck doesn't have 52 cards");
+    exit(EXIT_FAILURE);
+  }
+  card_t* temp_c[52];
+  deck_t* temp_d = {temp_c, 0};
+  for (int i = 0; i < 52; i++) {
+    if (!d->cards[i]) {
+      printf("Null card pointer");
+      exit(EXIT_FAILURE);
+    }
+    assert_card_valid(*(d->cards[i]));
+    if (deck_contains(temp_d, *(d->cards[i]))) {
+      printf("Duplicate card found");
+      exit(EXIT_FAILURE);
+    }
+    temp_d->cards[i] = *(d->cards[i]);
+    temp_d->n_cards++;
+  }
 }
